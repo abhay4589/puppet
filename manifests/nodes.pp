c6a7cc27a6806195c125859efa96af30dd5a6cb2
root <taj1@rocketmail.com>
2012-10-09 00:00:00 +0530

    All the remaining till %f

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -5,7 +5,11 @@ node rserv2	{
 	
 	file	{	"/etc/motd"		:	source	=> 'puppet:///files/motd',	}
 	file	{	"/root/.vimrc"	: source	=> 'puppet:///files/vimrc',	}
-	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/vimrc',	}
+	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	file	{	"/home/$USER/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	file	{	"/home/$USER/.vimrc"	:	source	=>	'puppet:///files/vimrc',	}
+	file	{	"/home/abhay/.vimrc"	:	source	=>	'puppet:///files/vimrc',	}
+	file	{	"/etc/resolv.conf"	:	source	=>	'puppet:///files/resolv.conf.generic',	}
 
 
 	}
@@ -17,3 +21,41 @@ node puppet	inherits rserv2	{
 
 node suse1	inherits rserv2	{
 	}
+
+node bt	inherits	rserv2	{
+
+$packages	= [	"nmap", "ttf-indic-fonts", "ttf-indic-fonts-core"	]
+	package	{	$packages: ensure => "installed"		}
+notify	{"Following $packages has been installed.":}
+}
+
+node	ubuntu	inherits	rserv2	{
+$packages	= [	"nmap", "ssh"]
+	package	{	$packages: ensure	=> "installed"	}
+notify	{"Following $packages has been installed.":}
+
+}
+
+node	omtechbuilds	{
+	file	{	"/etc/motd"		:	source	=> 'puppet:///files/motd',	}
+	file	{	"/root/.vimrc"	: source	=> 'puppet:///files/vimrc',	}
+	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	
+
+}
+
+node rvya1	{
+$packages = [ "nmap" ]
+	 package { $packages: ensure => "installed"    }
+notify  {"Following $packages has been installed.":}
+
+}
+
+node	storage1	{
+$packages	=	[ "vim" ]
+	package { $packages: ensure => "installed"    }
+	file	{	"/etc/motd"		:	source	=> 'puppet:///files/motd',	}
+	file	{	"/root/.vimrc"	: source	=> 'puppet:///files/vimrc',	}
+	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	notify  {"Following $packages has been installed.":}
+}
