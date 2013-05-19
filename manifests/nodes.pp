1994f9b6632740fc9665dc030388ad733f0eda9c
abhay4589 <taj1@rocketmail.com>
2013-05-20 04:13:15 +0530

    My another commit at 20 may 2013

--- manifests/nodes.pp
+++ manifests/nodes.pp
@@ -5,16 +5,31 @@ node rserv2	{
 	
 	file	{	"/etc/motd"		:	source	=> 'puppet:///files/motd',	}
 	file	{	"/root/.vimrc"	: source	=> 'puppet:///files/vimrc',	}
+	file	{	"/root/.zshrc"	: source	=> 'puppet:///files/zshrc',	}
+	file	{	"/root/.tmux.conf"	: source	=> 'puppet:///files/tmux.conf',	}
 	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	file	{	"/root/.bash_profile"	:	source	=>	'puppet:///files/bash_profile',	}
 	file	{	"/home/$USER/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	file	{	"/home/$USER/.bash_profile"	:	source	=>	'puppet:///files/bash_profile',	}
 	file	{	"/home/$USER/.vimrc"	:	source	=>	'puppet:///files/vimrc',	}
-	file	{	"/home/abhay/.vimrc"	:	source	=>	'puppet:///files/vimrc',	}
-	file	{	"/etc/resolv.conf"	:	source	=>	'puppet:///files/resolv.conf.generic',	}
-
-
-	}
-
-
+	file	{	"/home/$USER/.zshrc"	:	source	=>	'puppet:///files/zshrc',	}
+	file	{	"/home/$USER/.tmux.conf"	:	source	=>	'puppet:///files/tmux.conf',	}
+#		
+#
+#
+file { "/root/.oh-my-zsh" :
+	ensure => directory,
+	recurse => true,
+	purge => true,
+	force => true,
+	owner => "root",
+	group => "root",
+	mode => 0644,
+	source => "puppet:///files/oh-my-zsh",
+		
+}
+#
+}
 node puppet	inherits rserv2	{
 		
 	}
@@ -29,8 +44,9 @@ $packages	= [	"nmap", "ttf-indic-fonts", "ttf-indic-fonts-core"	]
 notify	{"Following $packages has been installed.":}
 }
 
-node	ubuntu	inherits	rserv2	{
-$packages	= [	"nmap", "ssh"]
+node	ubu1	inherits "rserv2" {
+include ssh
+$packages	= [	"nmap",  "zsh", "tmux" ]
 	package	{	$packages: ensure	=> "installed"	}
 notify	{"Following $packages has been installed.":}
 
@@ -58,4 +74,39 @@ $packages	=	[ "vim" ]
 	file	{	"/root/.vimrc"	: source	=> 'puppet:///files/vimrc',	}
 	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
 	notify  {"Following $packages has been installed.":}
+}
+
+node	bsd1	{
+$packages	=	[ "editors/vim","editors/nano" ]
+	package { $packages: ensure => "installed",  provider	=>	"freebsd",  }
+#	file	{	"/etc/motd"		:	source	=> 'puppet:///files/motd',	}
+#	file	{	"/root/.vimrc"	: source	=> 'puppet:///files/vimrc',	}
+#	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+#	file	{	"/root/.bash_profile"	:	source	=>	'puppet:///files/bash_profile',	}
+#	file 	{	"/root/.ssh/authorized_keys"			:	source	=> 'puppet:///files/ssh/authorized_keys',	}
+	#notify  {"Following $packages has been installed.":}
+}
+
+node bsd3	inherits bsd1 {
+
+
+}
+
+
+node openfire {
+#$packages = [ "vim","nano"  ]
+#	package { $packages: ensure => "installed", provider => "openbsd", }
+
+	file	{	"/etc/motd"		:	source	=> 'puppet:///files/motd',	}
+	file	{	"/root/.vimrc"	: source	=> 'puppet:///files/vimrc',	}
+	file	{	"/root/.zshrc"	: source	=> 'puppet:///files/zshrc',	}
+	file	{	"/root/.tmux.conf"	: source	=> 'puppet:///files/tmux.conf',	}
+	file	{	"/root/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	file	{	"/root/.bash_profile"	:	source	=>	'puppet:///files/bash_profile',	}
+	file	{	"/home/$USER/.bashrc"	:	source	=>	'puppet:///files/bashrc',	}
+	file	{	"/home/$USER/.bash_profile"	:	source	=>	'puppet:///files/bash_profile',	}
+	file	{	"/home/$USER/.vimrc"	:	source	=>	'puppet:///files/vimrc',	}
+	file	{	"/home/$USER/.zshrc"	:	source	=>	'puppet:///files/zshrc',	}
+	file	{	"/home/$USER/.tmux.conf"	:	source	=>	'puppet:///files/tmux.conf',	}
+
 }
